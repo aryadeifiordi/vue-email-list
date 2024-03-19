4a241a8a96a1a845ed2ca687cd6ac63098d9bd8b
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-19 18:42:18 +0100

    email list

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,25 @@
 console.log('works');
+//https://flynn.boolean.careers/exercises/api/random/mail
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            randomEmails: []
+        }
+    },
+    methods: {
+        callApi() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => { this.randomEmails.push(response.data.response) });
+            }
+            console.log(randomEmails);
+        }
+    },
+    mounted() {
+        this.callApi()
+    }
+}).mount('#app')
